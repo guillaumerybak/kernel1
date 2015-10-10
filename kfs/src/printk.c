8ad5a817068a0bd7049d48ddb64bbaa914c15893
Florian Champin <florian.champin@epitech.eu>
2015-10-10 23:07:33 +0200

    Comment function inactive

--- kfs/src/printk.c
+++ kfs/src/printk.c
@@ -5,7 +5,7 @@
 ** Login   <champi_f@champi-f>
 ** 
 ** Started on  Sat Oct 10 14:33:51 2015 Florian Champin
-** Last update Sat Oct 10 22:56:50 2015 Florian Champin
+** Last update Sat Oct 10 23:07:00 2015 Florian Champin
 */
 
 #include "system.h"
@@ -40,9 +40,6 @@ unsigned short *memsetw(void *dest, unsigned short val, size_t count)
 
 size_t strlen(const char *str)
 {
-  /* This loops through character array 'str', returning how
-   *  many characters it needs to check before it finds a 0.
-   *  In simple words, it returns the length in bytes of a string */
   size_t retval;
   for(retval = 0; str[retval] != '\0'; retval++)
     {}
@@ -51,16 +48,18 @@ size_t strlen(const char *str)
 
 /* We will use this later on for reading from the I/O ports to get data*  from devices such as the keyboard. We are using what is called
  *  'inline assembly' in these routines to actually do the work */
+/*
 unsigned char inportb (unsigned short _port)
 {
   unsigned char rv;
   __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
   return rv;
 }
-
+*/
 /* We will use this to write to I/O ports to send bytes to devices. this*  will be used in the next tutorial for changing the textmode cursor
  *  position. Again, we use some inline assembly for the stuff that simply
  *  cannot be done in C */
+
 void outportb (unsigned short _port, unsigned char _data)
 {
   __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
