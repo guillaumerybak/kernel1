4a55e5ab2ab7c759300a27de57f09deeb9969a77
Florian Champin <florian.champin@epitech.eu>
2015-10-12 19:44:22 +0200

    New version, write on VFB isOkay

--- kfs/src/printk.c
+++ kfs/src/printk.c
@@ -5,71 +5,44 @@
 ** Login   <champi_f@champi-f>
 ** 
 ** Started on  Sat Oct 10 14:33:51 2015 Florian Champin
-** Last update Sat Oct 10 23:07:00 2015 Florian Champin
+** Last update Mon Oct 12 19:40:47 2015 Florian Champin
 */
 
-#include "system.h"
+#include "vfb.h"
 
-/* You will need to code these up yourself!  */
-unsigned char *memcpy(void *dest, const void *src, size_t count)
-{
-  size_t i;
-  unsigned char *tmp = (unsigned char *)dest;
-  for (i = 0; i < count; i++)
-    tmp[i] = ((unsigned char *)src)[i];
-  return tmp;
-}
-
-unsigned char *memset(void *dest, unsigned char val, size_t count)
-{
-  size_t i;
-  unsigned char *tmp = (unsigned char *)dest;
-  for (i = 0; i < count; i++)
-    tmp[i] = val;
-  return tmp;
-}
-
-unsigned short *memsetw(void *dest, unsigned short val, size_t count)
-{
-  size_t i;
-  unsigned short *tmp = (unsigned short *)dest;
-  for (i = 0; i < count; i++)
-    tmp[i] = val;
-  return tmp;
-}
-
-size_t strlen(const char *str)
-{
-  size_t retval;
-  for(retval = 0; str[retval] != '\0'; retval++)
-    {}
-  return retval;
-}
-
-/* We will use this later on for reading from the I/O ports to get data*  from devices such as the keyboard. We are using what is called
- *  'inline assembly' in these routines to actually do the work */
-/*
 unsigned char inportb (unsigned short _port)
 {
   unsigned char rv;
   __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
   return rv;
 }
-*/
-/* We will use this to write to I/O ports to send bytes to devices. this*  will be used in the next tutorial for changing the textmode cursor
- *  position. Again, we use some inline assembly for the stuff that simply
- *  cannot be done in C */
 
 void outportb (unsigned short _port, unsigned char _data)
 {
   __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
 }
 
-/* This is a very simple main() function. All it does is sit in any*  infinite loop. This will be like our 'idle' loop */
+#define PORT 0x3f8 /* COM1 */
+
+void init_serial()
+{
+  outportb(PORT + 0, 0x03);
+  outportb(PORT + 3, 0x03);
+}
+
 void main()
 {
-  init_video();
-  cls();
-  puts("TEST\n");
+  vfb_clear();
+  int i = 0;
+  char l = 48;
+  for (i = 0; i < 20000; ++i) {
+    vfb_putstr(&l, 1);
+    ++l;
+    if (l > 90)
+      l = 48;
+    unsigned int j;
+    for (j = 0; j <  1083647; ++j){
+    }
+  }
   for (;;);
 }
---
+++ kfs/src/vfb.c
@@ -0,0 +1,56 @@
+/*
+** vfb.c for vfb in /home/champi_f/Kernel/ProjectOne/fromScratch/src
+** 
+** Made by Florian Champin
+** Login   <champi_f@champi-f>
+** 
+** Started on  Mon Oct 12 13:17:38 2015 Florian Champin
+** Last update Mon Oct 12 19:39:42 2015 Florian Champin
+*/
+
+#include "vfb.h"
+
+static struct s_vfb vfb = {
+  .pos = { .x = 0, .y = 0},
+  .color = 0x07,
+  .buffer = VBF_ADD,
+};
+
+static inline void	scroll_up() {
+  int	i;
+  for (i = 0; i < (NB_LINE - 1) * NB_COL; ++i) {
+    vfb.buffer[i] = vfb.buffer[i + NB_COL];
+  }
+
+  for (; i < NB_LINE * NB_COL; ++i)
+    vfb.buffer[i] = (vfb.color << 8) | ' ';
+
+  vfb.pos.y--;
+}
+
+static inline void	new_line() {
+  vfb.pos.x = 0;
+  vfb.pos.y++;
+  if (vfb.pos.y > NB_LINE - 1)
+    scroll_up();
+}
+
+static inline void	vfb_putchar(char c) {
+  vfb.buffer[vfb.pos.y * NB_COL + vfb.pos.x] = (vfb.color << 8) | c;
+  vfb.pos.x++;
+  if (vfb.pos.x == NB_COL)
+    new_line();
+}
+
+void	vfb_putstr(const char *str, int size) {
+  int	ite;
+
+  for (ite = 0; ite < size; ++ite)
+    vfb_putchar(str[ite]);
+}
+
+void	vfb_clear() {
+  memset(vfb.buffer, 0, 25 * 80 * 2);
+  vfb.pos.x = 0;
+  vfb.pos.y = 0;
+}
