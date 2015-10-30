adf300a5cfdce9687f7cef996cdddd687d577593
rybak_g <rybak_g@epitech.eu>
2015-10-29 21:01:41 -0700

    update syscall

--- syscall/sys_uptime.c
+++ syscall/sys_uptime.c
@@ -1,12 +1,19 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 
-SYSCALL_DEFINE1(uptime, time_t __user *, in)
+static time_t do_uptime(time_t __user *in)
 {
 	static struct timespec uptime;
 
 	get_monotonic_boottime(&uptime);
-	if (in)
-		*in = (time_t)uptime.tv_sec;
-	return *in;
+	if (in) {
+		if (copy_to_user(in, &uptime.tv_sec, sizeof(*in)))
+			return ((time_t) -1);
+	}
+	return uptime.tv_sec;
+}
+
+SYSCALL_DEFINE1(uptime, time_t __user *, in)
+{
+	return do_uptime(in);
 }
